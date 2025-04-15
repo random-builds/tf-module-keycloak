8ceaef8639165bad2b390876cec4c5afddde2c7f
ifiz <ifiz@pm.me>
2025-04-15 03:08:53 -0400

    update outputs

--- openid-client/outputs.tf
+++ openid-client/outputs.tf
@@ -1,3 +1,4 @@
-output "openid_client_details" {
-  value = {for client in keycloak_openid_client.openid_client: client.client_id => client.client_secret}
+output "openid_client_secret" {
+  value = keycloak_openid_client.this.client_secret
+  sensitive = true
 }
